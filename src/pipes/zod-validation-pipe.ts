9b6029359ffdde40c3e7902689219f604a4db050
dopeeydev <italodopeey@gmail.com>
2026-02-28 06:41:47 -0300

    feat: implement account creation with validation and password hashing

---
+++ src/pipes/zod-validation-pipe.ts
@@ -0,0 +1,29 @@
+import {
+  PipeTransform,
+  ArgumentMetadata,
+  BadRequestException,
+} from '@nestjs/common'
+import { ZodError, ZodSchema } from 'zod'
+import { fromZodError } from 'zod-validation-error'
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodSchema) {}
+
+  transform(value: any, metadata: ArgumentMetadata) {
+    try {
+      this.schema.parse(value)
+    } catch (error) {
+      if (error instanceof ZodError) {
+        throw new BadRequestException({ 
+          message: 'Validation Failed', 
+          statusCode: 400,
+          errors: fromZodError(error),
+        })
+      }
+
+      throw new BadRequestException('Validation Failed')
+    }
+    
+    return value
+  }
+}
