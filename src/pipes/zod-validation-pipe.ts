b5d34b27f9fe32e00c6617193e422a2bab0ff2ac
dopeeydev <italodopeey@gmail.com>
2026-03-06 11:27:56 -0300

    feat(questions): add FetchRecentQuestionsController and implement pagination for fetching recent questions; update ZodValidationPipe to remove unused metadata

--- src/pipes/zod-validation-pipe.ts
+++ src/pipes/zod-validation-pipe.ts
@@ -1,6 +1,5 @@
 import {
   PipeTransform,
-  ArgumentMetadata,
   BadRequestException,
 } from '@nestjs/common'
 import { ZodError, ZodSchema } from 'zod'
@@ -9,9 +8,9 @@ import { fromZodError } from 'zod-validation-error'
 export class ZodValidationPipe implements PipeTransform {
   constructor(private schema: ZodSchema) {}
 
-  transform(value: any, metadata: ArgumentMetadata) {
+  transform(value: any) {
     try {
-      this.schema.parse(value)
+      return this.schema.parse(value)
     } catch (error) {
       if (error instanceof ZodError) {
         throw new BadRequestException({ 
