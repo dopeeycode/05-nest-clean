48bda72a1574762193bd9c747bb99a588c41ea27
dopeeydev <italodopeey@gmail.com>
2026-03-07 12:30:17 -0300

    feat: add dotenv configuration and setup for e2e tests; update tsconfig and vitest config

---
+++ test/setup-e2e.ts
@@ -0,0 +1,44 @@
+import 'dotenv/config'
+
+import { execSync } from 'node:child_process'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from 'prisma/generated/prisma/client'
+import { randomUUID } from 'node:crypto'
+
+
+const databaseURL = process.env.DATABASE_URL as string
+const schema = new URL(databaseURL).searchParams.get('schema')
+
+const adapter = new PrismaPg(
+  { connectionString: databaseURL },
+  { schema: schema ?? 'public' },
+)
+
+const prisma = new PrismaClient({ adapter })
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL environment variable is not set')
+}
+
+function generateUniqueDatabaseURL(schemaId: string): string {
+  const url = new URL(databaseURL)
+  url.searchParams.set('schema', schemaId)
+
+  return url.toString()
+}
+
+const schemaId = randomUUID()
+
+
+beforeAll(async () => {
+  const databaseURL = generateUniqueDatabaseURL(schemaId)
+
+  process.env.DATABASE_URL = databaseURL
+
+  execSync('pnpm prisma migrate deploy')
+})
+
+afterAll(async () => {
+  await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId}" CASCADE`)
+  await prisma.$disconnect()
+})
